631870cddf7a08a82b129949ef16632668797940
jman005 <justindev@protonmail.com>
2019-07-24 00:41:20 -0400

    Opening files

--- lightext/widgets/editor.py
+++ lightext/widgets/editor.py
@@ -1,5 +1,8 @@
 from PyQt5.QtWidgets import (QTextEdit, QWidget, QTabWidget, QFileDialog, QVBoxLayout)
 
+def filename_from_path(path):
+    return path.split("/")[-1]
+
 class TabWindow(QWidget):
     def __init__(self):
         super(TabWindow, self).__init__()
@@ -17,12 +20,20 @@ class TabWindow(QWidget):
         tab = self.tabs.currentWidget()
 
         if tab.path is None:
-            path, _ = QFileDialog.getSaveFileName(self, "Save as...", "", "Plaintext (.txt)")
+            path, _ = QFileDialog.getSaveFileName(self, "Save as...", "", "Plaintext (*.txt)")
             if path:
                 tab.save(path)
+                self.tabs.setTabText(self.tabs.currentIndex(), filename_from_path(path))
 
         else:
             tab.save(tab.path)
+
+    def open(self):
+        tab = self.tabs.currentWidget()
+        path, _ = QFileDialog.getOpenFileName(self, caption="Open file...")
+        if path:
+            tab.open(path)
+            self.tabs.setTabText(self.tabs.currentIndex(), filename_from_path(path))
 
 class TextEditor(QTextEdit):
 
@@ -35,3 +46,10 @@ class TextEditor(QTextEdit):
             f.write(self.toPlainText())
 
         self.path = location
+
+    def open(self, location):
+        with open(location, "r") as f:
+            self.setPlainText(f.read())
+
+        self.path = location
+
--- lightext/widgets/lightext_main.py
+++ lightext/widgets/lightext_main.py
@@ -7,6 +7,7 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
 
         self.resize(800,600)
+        self.setWindowTitle("Lightext")
 
         editor = TabWindow()
         self.setCentralWidget(editor)
@@ -14,17 +15,14 @@ class MainWindow(QMainWindow):
         saveAction = QAction("Save", self)
         saveAction.triggered.connect(editor.save)
 
+        openAction = QAction("Open", self)
+        openAction.triggered.connect(editor.open)
+
         menubar = self.menuBar()
         fileMenu = menubar.addMenu("File")
         fileMenu.addAction(saveAction)
+        fileMenu.addAction(openAction)
 
 
 
         self.show()
-
-
-
-
-
-
-
